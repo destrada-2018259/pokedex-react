147a2affb8fac9381ea3f6fdca04e30adae5e4e0
Daniel Rojo <danielgworojo@gmail.com>
2024-05-06 08:11:40 -0600

    infinite scroll

--- components/Pokemons.jsx
+++ components/Pokemons.jsx
@@ -1,40 +1,30 @@
 import React from 'react'
 import './Pokemons.css'
+import './PokemonCard.css'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-import { useEffect, useState } from 'react'
+
 import { PokemonCard } from './PokemonCard';
+import usePokemons from '../src/hooks/usePokemons';
+import { Loading } from './Loading';
 
 export const Pokemons = () => {
-    const [pokemons, setPokemons] = useState([ ]);
 
-    useEffect(() => {
-  
-      const getPokemon = async () => {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=40&offset=0');
-        const pokemonList = await response.json();
-        const {results} = pokemonList;
-       
-        const newPokemons = results.map(async(pokemon) => {
-  
-          const response = await fetch(pokemon.url);
-          const poke = await response.json()
-  
-          return {
-            id: poke.id,
-            name: poke.name,
-            img: poke.sprites.other.dream_world.front_default
-          }
-        })
-        setPokemons(await Promise.all(newPokemons))
-      }
-  
-      getPokemon();
-    }, [])
-
+    const {pokemons, morePokemon, viewMore} = usePokemons()
+   
   return (
-    <section className='pokemon-container'>
-        {pokemons.map(pokemon => <PokemonCard {...pokemon} />)}
-    </section>
+    <InfiniteScroll 
+        dataLength={pokemons.length}
+        next={morePokemon}
+        hasMore={viewMore}
+        loader={ <Loading/> }
+        endMessage={
+            <h3 className='title' style={{gridColumn: '1/6'}}>Sorry, there aren't more Pokemon to show</h3>
+        }
+        className='pokemon-container'
+    >
+        {pokemons.map(pokemon => <PokemonCard {...pokemon} key ={pokemon.id} />)}
+    </InfiniteScroll>
   )
 }
 
---
+++ src/hooks/usePokemons.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+const URL_DEFAULT = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0'
+
+export default function usePokemons(){
+    const [pokemons, setPokemons] = useState([ ]);
+    const [nextUrl, setNextUrl] = useState('')
+    const [viewMore, setViewMore] = useState(true)
+   
+    const getPokemon = async (url = URL_DEFAULT) => {
+
+        const response = await fetch(url);
+        const pokemonList = await response.json();
+        const {next, results} = pokemonList;
+       
+        const newPokemons = await Promise.all(
+            results.map( async(pokemon) =>{
+                const response = await fetch(pokemon.url)
+                const poke = await response.json()
+
+                return{
+                    id: poke.id,
+                    name: poke.name,
+                    img: poke.sprites.other.dream_world.front_default || poke.sprites.front_default
+                }
+            })
+        )
+        return { next, newPokemons}
+    }
+
+    const obtainPokemons = async () =>{
+        const {next, newPokemons} = await getPokemon()
+        setPokemons(newPokemons)
+        setNextUrl(next)
+    }
+
+    const morePokemon = async() =>{
+        const {next, newPokemons} = await getPokemon(nextUrl)
+        setPokemons(prev => [...prev, ...newPokemons])
+        next === null && setViewMore(false)
+        setNextUrl(next)
+    }
+
+    useEffect(() => {obtainPokemons()}, [])
+
+    return {pokemons, morePokemon, viewMore}
+}
