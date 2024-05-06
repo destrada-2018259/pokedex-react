2effd8639ae6e4adb260da6e6dd9ccc30f0faea9
Daniel Rojo <danielgworojo@gmail.com>
2024-05-05 18:54:31 -0600

    Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Logo, Moon, Sun } from './Icons'
+import "./Navbar.css"
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+        <nav>
+
+            <Logo />
+            <div className='switch'>
+              <Sun/>
+                <label>
+                    <input type="checkbox" className='check-switch' hidden/>
+                    <span className='slider'></span>
+                </label>
+              <Moon />
+            </div>
+        </nav>
+    </>
+
   )
 }
